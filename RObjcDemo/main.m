6226bfe90efbda15adb2062c26c6da5d7c6cb95f
Peter Pan <415391360@qq.com>
2017-10-31 11:29:48 +0800

    Initial Commit

---
+++ RObjcDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RObjcDemo
+//
+//  Created by panshen on 2017/10/31.
+//  Copyright © 2017年 panshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
